bc1d82733318ee3a721d2a6a50d27bd33b81230b
Harry Freeborough <harry@hfreeb.com>
2020-04-08 17:15:16 +0100

    Set firefox as the default browser

--- home/default.nix
+++ home/default.nix
@@ -25,4 +25,15 @@
     EDITOR = "nvim";
     TERMINAL = "termite";
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = let
+      browser = "firefox.desktop";
+    in {
+      "text/html" = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+    };
+  };
 }
