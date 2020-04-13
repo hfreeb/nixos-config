626fcb6fde079dbbe6c22d0302b4767745a17e5c
Harry Freeborough <harry@hfreeb.com>
2020-04-13 12:33:03 +0100

    Fix comment

--- common/default.nix
+++ common/default.nix
@@ -7,7 +7,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.nameservers = [ "9.9.9.9" "149.112.112.112" ];
-  # Generate read-only resolv.conf to stop dhcpcd overwriting it.
+  # Generate read-only resolv.conf to stop dhcpcd appending to it.
   environment.etc."resolv.conf".text = ''
     ${lib.concatStringsSep "\n" (map (ns: "nameserver ${ns}") config.networking.nameservers)}
     options edns0
