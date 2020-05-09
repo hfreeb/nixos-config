d3ff94f025fc166fb4219ae8e4187c272ba0d1bd
Harry Freeborough <harry@hfreeb.com>
2020-05-09 16:39:34 +0100

    Remove auto-gc
    The auto-gc meant that every week I had to recompile the cross-compiler
    for my kernel.

--- common/default.nix
+++ common/default.nix
@@ -118,11 +118,6 @@ in {
   };
   security.sudo.extraConfig = "Defaults lecture=never";
 
-  nix.gc = {
-    automatic = true;
-    dates = "Mon *-*-* 00:00:00";
-  };
-
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
